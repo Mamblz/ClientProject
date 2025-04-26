a18c58b9e920dfa4c5a780cacb9996fceef2ef94
kobza <kobza0406@gmail.com>
2025-04-26 13:28:23 +0300

    Добавьте файлы проекта.

---
+++ ClientProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Newtonsoft.Json;
+using ClientProject.Models;
+
+namespace ClientProject
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:7245");
+            Console.WriteLine("Hello, World!");
+            HttpResponseMessage message = await client.GetAsync("deviceslist");
+            string str = await message.Content.ReadAsStringAsync();
+            List<Device> device = JsonConvert.DeserializeObject<List<Device>>(str);
+        }
+    }
+}
